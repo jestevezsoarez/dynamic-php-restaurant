df75135fb632380304581f9a1c040df289ac7faf
Javier Estevez <javierdaniloestevez@gmail.com>
2021-08-13 03:45:45 -0300

    starting project, added styles and includes

---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<?php
+	include('includes/arrays.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body id="final-example">
+
+	<div class="wrapper">
+		
+		<div id="banner">
+			<a href="/" title="Return to Home">
+				<img src="img/banner.png" alt="Franklin's Fine Dining">
+			</a>
+		</div><!-- End banner -->
+
+		<div id="nav">
+			<?php include('includes/nav.php'); ?>
+		</div> <!-- End nav -->
+
+		<div class="content">
