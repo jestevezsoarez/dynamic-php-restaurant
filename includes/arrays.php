6273aa7ff7b5f4cabd12283dcdb32a6d098b535d
Javier Estevez <javierdaniloestevez@gmail.com>
2021-08-18 02:57:28 -0300

    added menu.php and the data in the array to be pulled from php with the different dishes

--- includes/arrays.php
+++ includes/arrays.php
@@ -54,4 +54,48 @@
 						)
 				   );
 
+
+	// Menu Items
+
+	$menuItems = array(
+
+					"club-sandwich" => array(
+
+						"title"  => "Club Sandwich",
+						"price"  => 11,
+						"blurb"  => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non elit lobortis, maximus est ut, suscipit est. Nunc id pretium ex. Praesent sit amet ante scelerisque, pulvinar justo sed, tempus risus. Aenean vel placerat erat. Praesent interdum sollicitudin dui, ut tincidunt sapien consectetur a. Nullam sit amet enim non urna tempus luctus sit amet congue est. Etiam rutrum facilisis leo, non sagittis nunc faucibus nec. Vestibulum sit amet elit at mi feugiat imperdiet ut nec arcu. Aliquam erat volutpat. Quisque consectetur ultricies mattis. Praesent tincidunt justo nisl, sit amet malesuada dolor tincidunt at. Nunc tempor, nibh ac suscipit pharetra, nibh ligula efficitur massa, eget tempus orci quam id sem. In accumsan lacus ultrices dolor auctor cursus. In mattis urna quis pulvinar dictum. Praesent ut condimentum orci. Curabitur ut quam enim.",
+						"drink"  =>  "Club Soda"
+
+					),
+
+					"dill-salmon" => array(
+
+						"title"  => "Lemon &amp; Dill Salmon",
+						"price"  => 18,
+						"blurb"  => "Sed at neque at elit pellentesque maximus ut quis nulla. Donec facilisis feugiat tincidunt. Duis ut lacinia velit. Nullam tincidunt justo ut augue fermentum ultrices. Ut lobortis, felis et commodo volutpat, turpis urna pellentesque turpis, ac convallis nisl sapien quis neque. Aenean placerat magna et metus fringilla feugiat. Praesent vel iaculis tortor. Sed cursus nisi arcu, ac sagittis velit luctus ut. Nulla facilisi. Nunc eget tempor quam, sit amet suscipit ex. Aenean fringilla, justo at porttitor bibendum, libero mi placerat ante, eu posuere elit arcu sit amet orci. Cras a sem leo. Aliquam nisi quam, finibus sit amet elit lobortis, mollis sodales diam. Aenean pellentesque dignissim sapien, et sollicitudin ex tincidunt sed. Curabitur fermentum lacus sit amet dolor sodales, at venenatis lorem tristique. Pellentesque eget leo id nulla feugiat malesuada.",
+						"drink"  =>  "Fancy Wine"
+
+					),
+
+					"super-salad" => array(
+
+						"title"  => "The Super Salad<sup>&reg;</sup>",
+						"price"  => 34,
+						"blurb"  => "Suspendisse purus risus, tincidunt quis faucibus non, malesuada a ligula. Nam volutpat ex ut cursus congue. Quisque tincidunt tincidunt justo sit amet ultrices. Sed turpis nibh, lacinia et orci eu, sagittis lacinia elit. In eget augue felis. Aliquam ut lorem a sapien dapibus elementum. Etiam euismod, metus id rhoncus pulvinar, sem nisi rhoncus lorem, at consequat ex velit a arcu. Suspendisse potenti. Donec consectetur volutpat est id imperdiet. Maecenas ultrices nibh et mi tempus efficitur ut nec ipsum. In sed tincidunt orci. Morbi sit amet felis nec nulla interdum aliquet sit amet vel mauris. Pellentesque luctus sagittis sollicitudin. Vestibulum a porttitor ante. Morbi lobortis pellentesque dapibus. Phasellus maximus cursus odio sit amet facilisis.",
+						"drink"  =>  "Jug o' Water"
+
+					),
+
+					"mexican-barbacoa" => array(
+
+						"title"  => "Mexican Barbacoa",
+						"price"  => 23,
+						"blurb"  => "Maecenas quis sollicitudin augue, a euismod neque. Etiam dapibus arcu urna, sed finibus dolor imperdiet a. Donec mauris purus, hendrerit vitae eros eleifend, luctus volutpat nisi. Duis imperdiet eros sed mi pharetra posuere. Quisque sed cursus odio, eu semper orci. Praesent tincidunt massa auctor lacus commodo vulputate. Vivamus facilisis non justo eget dictum. Donec purus sapien, bibendum vel est sed, eleifend egestas lectus. Sed interdum eleifend elit. Suspendisse auctor commodo faucibus. Vivamus id finibus ligula. Suspendisse tempor libero ligula, ac hendrerit dolor aliquam ac. Aliquam dictum sapien sed nunc vehicula pretium.",
+						"drink"  =>  "Beer with a lime"
+
+					),
+
+
+				);
+
 ?>
