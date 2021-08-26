a8972d2aaac7d449b142f0afaae8b4239eee57a8
Javier Estevez <javierdaniloestevez@gmail.com>
2021-08-26 01:09:23 -0300

    implemented send email in contact.php

--- contact.php
+++ contact.php
@@ -72,16 +72,50 @@
 					die(); // If true, kill the script
 				}
 
+				// Check if fields were empty
 				if (!$name || !$email || !$msg ) {
 
 					echo '<h4 class="error">All fields required.</h4><a href="contact.php" class="button block">Go back and try again</a>';
 					exit;
 				}
 
-			}
+				// Add the recipient email to a variable
+				$to = "your@email.com";
 
+				// Create a subject
+				$subject = "$name sent you a meesage via your contact form";
 
+				// Construct the message
+				$message  = "Name: $name\r\n";
+				$message .= "Email: $email\r\n";
+				$message .= "Message:\r\n$msg";
+
+				// Check if the subscribe checkbox was chequed
+				if (isset($_POST['subscribe']) && $_POST['subscribe'] == 'Subscribe') {
+
+					// Add a new line to the $message
+					$message .= "\r\n\r\nPlease add $email to the mailing list.\r\n";
+				}
+
+				$message = wordwrap($message, 72);
+
+				// Set the mail headers into a variable
+				$headers  = "MIME-Version: 1.0\r\n";
+				$headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+				$headers .= "From: $name <$email> \r\n";
+				$headers .= "X-Priority: 1\r\n";
+				$headers .= "X-MSMail-Priority: High\r\n\r\n";
+
+				// Send the email
+				mail( $to, $subject, $message, $headers );
 		?>
+
+		<!-- Show succes message after email has sent -->
+		<h5>Thanks for contacting Franklin's!</h5>
+		<p>Please allow 24 hours for a response.</p>
+		<p><a href="/final" class="button block">&laquo; Go to Home Page</a></p>
+
+		<?php } else { ?>
 
 		<h1>Get in touch with us!</h1>
 
@@ -102,6 +136,10 @@
 			<input type="submit" class="button next" name="contact_submit" value="Send Message">
 			
 		</form>
+
+		<?php } ?>
+
+		<hr>
 		
 	</div><!-- contact -->
 
